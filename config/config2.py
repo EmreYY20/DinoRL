7c1631ef61e861d832c8c4bc42b3069511663dbc
EmreYY20 <120115560+EmreYY20@users.noreply.github.com>
2024-06-10 11:19:50 +0200

    update config

--- config/config2.py
+++ config/config2.py
@@ -16,27 +16,27 @@ args = {
     "img_cols": 80,
     "img_channels": 4, 
     "checkpoint" : "./weights/double_dqn2.pth",
-    "SAVE_EVERY": 100, # Save model every 100 episodes
+    "SAVE_EVERY": 1000, # Save model every 1000 episodes
     "num_test_episode": 20, # Number of test episodes
-    "TEST_EVERY": 100, # Test the model every 100 episodes
+    "TEST_EVERY": 1000, # Test the model every 1000 episodes
     "SAVE_GIF": False, # Save GIF of the gameplay
     "SLEEP": 0.007, # Using sleep() to control the FPS in testing
 
     # Hyperparameters
     "algorithm": "DoubleDQN", # Algorithm to use
-    "EPISODE": 200, # Number of episodes for training
+    "EPISODE": 2500, # Number of episodes for training
     "ACTIONS": 2, # Possible actions: jump or do nothing
     "GAMMA": 0.99, # Decay rate of past observations
-    "OBSERVATION": 50, # Timesteps to observe before training
+    "OBSERVATION": 500, # Timesteps to observe before training
     "FINAL_EPSILON": 1e-1, # Final value of epsilon (exploration probability)
     "INITIAL_EPSILON": 1, # Starting value of epsilon (initial randomness)
     "EPSILON_DECAY": 0.9999925, # Decay rate for epsilon
-    "REPLAY_MEMORY": 50, # Number of previous transitions to remember
+    "REPLAY_MEMORY": 1000, # Number of previous transitions to remember
     "BATCH": 32, # Size of minibatch
     "FRAME_PER_ACTION": 1, # Number of frames per action
     "lr": 1e-4, # Learning rate
     "weight_decay": 1e-4, # Weight decay for regularization
-    "SYNC_EVERY": 100, # Number of experiences between Q_target & Q_online sync
+    "SYNC_EVERY": 1000, # Number of experiences between Q_target & Q_online sync
     "TRAIN_EVERY": 3, # Train the model every 3 steps
     "prioritized_replay": True, # FPS is slower than unprioritized
     "grad_norm_clipping": 10 # Gradient clipping to prevent explosion
